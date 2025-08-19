9c9e2f12d2b1ae95537730be7d359e386399ef12
Jim Rauch <jim@autalic.ai>
2025-08-19 14:44:10 +1200

    Update telnyx.js

--- api/telnyx.js
+++ api/telnyx.js
@@ -1,5 +1,8 @@
 const nodemailer = require("nodemailer");
 
+// Force rebuild - using SMTP_USER as sender
+console.log("Starting webhook handler...");
+
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
